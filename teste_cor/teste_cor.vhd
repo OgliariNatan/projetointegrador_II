6b38a4f075fb96426a9ee11895632e37a421c7e6
gutovsk49 <augusto.d.rodrigues@hotmail.com>
2017-10-18 10:42:08 -0200

    adicionado projeto de teste do sensor de cor ao diretorio do projeto integrador

---
+++ teste_cor/teste_cor.vhd
@@ -0,0 +1,30 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+
+
+
+ENTITY teste_cor IS
+
+	PORT	 ( 
+			  );
+			  
+END teste_cor;
+
+
+
+
+ARCHITECTURE behavior OF teste_cor IS
+
+	
+
+	BEGIN
+	
+	PROCESS()
+	
+	
+	BEGIN
+	
+	END PROCESS;
+	
+END;
